d42db9a4972a9d6e688dc5ec862dad5698877222
bomba-cat <bledion.morina@hotmail.com>
2024-05-20 23:12:42 +0200

    added comments to C++

--- C++/program.cpp
+++ C++/program.cpp
@@ -6,12 +6,15 @@ int main() {
 	int range;
 	int mark;
 
+	// Get user input
 	cout << "range: ";
 	cin >> range;
 	cout << "mark every: ";
 	cin >> mark;
 
+	// Loop
 	for (int num = 1; num <= range; num++) {
+		// Check if mark is hit
 		if (num % mark == 0) {
 			cout << num << " <-\n";
 		} else {
